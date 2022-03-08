12acd2b2a0f3ce8aac3d3166118e68fbcbf04fc0
AlvaroRS93 <86481086+AlvaroRS93@users.noreply.github.com>
2022-03-08 17:38:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var escondido = false;
+
+esconderSobreMim = function(){
+    var sobreMim = document.getElementById("conteudo");
+    this.escondido = !this.escondido;
+    sobreMim.hidden = this.escondido;
+    console.log(sobreMim.hidden);
+}
